9c36736aa0b677e13cc9cdeb8642a9b9b740d583
Sergey Kruk <sergey.kruk@gmail.com>
2019-12-26 20:30:14 +0100

    Better code readability

--- src/prog.s
+++ src/prog.s
@@ -1,101 +1,103 @@
-rwa .equ $600f
-rwb .equ $6000
+via_data_a .equ $600f
+via_data_b .equ $6000
+via_dir_a .equ $6003
+via_dir_b .equ $6002
 btnstate .equ $3000
 
   .org $8000
 
 reset:
-  lda #$e1
-  sta $6003
-  lda #$ff
-  sta $6002
+  lda #%11100001 ; LCD control (E, RW, RS) and LED
+  sta via_dir_a
+  lda #%11111111 ; LCD data
+  sta via_dir_b
 
   lda #$0f
   jsr waitms
-  lda #$38
+  lda #%00111000 ; 8 bit; 2 lines; 5x8 dots
   jsr lcddir
   lda #$05
   jsr waitms
-  lda #$38
+  lda #%00111000 ; repeat 3 times
   jsr lcddir
   lda #$01
   jsr waitms
-  lda #$38
+  lda #%00111000
   jsr lcddir
   lda #$01
   jsr waitms
 
-  lda #$01
+  lda #%00000001 ; clear display
   jsr lcddir
   lda #$01
   jsr waitms
   jsr lcdbusy
-  lda #$0f
+  lda #%00001111 ; display on; cursor on; blink on
   jsr lcddir
   jsr lcdbusy
-  lda #$02
+  lda #%00000010 ; lcd home
   jsr lcddir
   jsr lcdbusy
-  lda #$06
+  lda #%00000110 ; increment address; no shift display
   jsr lcddir
   jsr lcdbusy
 
-  lda #$24
+  lda #"$"
   jsr lcdprnt
 
-  lda #$3e
+  lda #">"
   jsr lcdprnt
 
-  lda rwa
+  lda via_data_a
   and btnmask
   sta btnstate
 
 loop:
-  lda rwa
+  lda via_data_a
   and btnmask
   cmp btnstate
   beq loop
   sta btnstate
   lda #$09
   jsr waitms
-  lda rwa
+  lda via_data_a
   and btnmask
   cmp btnstate
   bne loop
   bit btnup
   bne noupbtn
-  lda #$55
+  lda #"u"
   jsr lcdprnt
   lda #$01
-  sta rwa
+  sta via_data_a
   jmp loop
 noupbtn:
   bit btndown
   bne nodownbtn
-  lda #$44
+  lda #"d"
   jsr lcdprnt
   lda #$01
-  sta rwa
+  sta via_data_a
   jmp loop
 nodownbtn:
   bit btnleft
   bne noleftbtn
-  lda #$4c
+  lda #"l"
   jsr lcdprnt
   lda #$01
-  sta rwa
+  sta via_data_a
   jmp loop
 noleftbtn:
   bit btnright
   bne norightbtn
-  lda #$52
+  lda #"r"
   jsr lcdprnt
   lda #$01
-  sta rwa
+  sta via_data_a
   jmp loop
 norightbtn:
   lda #$00
-  sta rwa
+  sta via_data_a
   jmp loop
 
 waitms:
@@ -113,13 +115,13 @@ lcddir:
   pha
   pha
   lda lcdsys
-  sta rwa
+  sta via_data_a
   ora lcde
-  sta rwa
+  sta via_data_a
   pla
-  sta rwb
+  sta via_data_b
   lda lcdsys
-  sta rwa
+  sta via_data_a
   pla
   rts
 
@@ -127,31 +129,31 @@ lcdprnt:
   pha
   pha
   lda lcdtxt
-  sta rwa
+  sta via_data_a
   ora lcde
-  sta rwa
+  sta via_data_a
   pla
-  sta rwb
+  sta via_data_b
   lda lcdtxt
-  sta rwa
+  sta via_data_a
   jsr lcdbusy
   pla
   rts
 
 lcdbusy:
   pha
-  lda #$7f
-  sta $6002
+  lda #%01111111
+  sta via_dir_b
 lcdbusyloop0:
   lda lcdrw
-  sta rwa
+  sta via_data_a
   ora lcde
-  sta rwa
-  lda rwb
-  and #$80
+  sta via_data_a
+  lda via_data_b
+  and lcdbusyflag
   bne lcdbusyloop0
-  lda #$ff
-  sta $6002
+  lda #%11111111
+  sta via_dir_b
   pla
   rts
 
@@ -163,6 +165,8 @@ lcde:
   .byte $80
 lcdrw:
   .byte $40
+lcdbusyflag:
+  .byte $80
 
 btnup:
   .byte $02
